21cf2c19687acec087c76a859f29533fa33fc20b
amenotu <kyabut@gmail.com>
2020-09-09 15:45:16 -0700

    Added util function that sends a response to a mock POST

---
+++ src/utils/mockPost.ts
@@ -0,0 +1,48 @@
+interface Response {
+  status: number;
+  statusText: string;
+  qualificationStatus: null | boolean;
+  dqMsg?: string;
+}
+
+interface Request {
+  url: string;
+  method: string;
+  body: string;
+  headers: {};
+  mode: string;
+  redirect: string;
+  referrerPolicy: string;
+}
+
+export const mockPost = (requestObj: Request) => {
+  console.log("REQUEST OBJECT FROM MOCKPOST: ", requestObj);
+
+  const parsedRequestObj = JSON.parse(requestObj.body);
+  const aFifthOfEYI = parsedRequestObj.estimatedYearlyIncome / 5;
+
+  let response: Response = {
+    status: 200,
+    statusText: "Success",
+    qualificationStatus: null,
+  };
+
+  if (parsedRequestObj.autoPurchasePrice > 1000000) {
+    response.status = 400;
+    response.statusText = "Bad Request";
+  } else {
+    if (
+      parsedRequestObj.autoPurchasePrice > aFifthOfEYI ||
+      parsedRequestObj.estimatedCreditScore < 600
+    ) {
+      response.qualificationStatus = false;
+      response.dqMsg = "some Lorem Ispsum";
+    } else {
+      response.qualificationStatus = true;
+    }
+  }
+
+  return new Promise<Response>((resolve) => {
+    resolve(response);
+  });
+};
