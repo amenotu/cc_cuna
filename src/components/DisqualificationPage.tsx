28b3c88406c5a7f19e969a9823f1684dcea06875
amenotu <kyabut@gmail.com>
2020-09-09 20:14:35 -0700

    Added disqualification message from AppContext

--- src/components/DisqualificationPage.tsx
+++ src/components/DisqualificationPage.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import AppContext from "../contexts/AppContext";
 
 export default function DisqualificationNotice() {
+  const { disqualificationMessage } = useContext(AppContext);
+
   return (
     <div>
-      <p>Some lorem ipsum from the mock API about being disqualified</p>
+      <p>{disqualificationMessage}</p>
       <p>
         Please contact customer service at 1-800-XXX-XXXX for further
         assistance.
